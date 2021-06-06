7f56486bf36a7cfc042d105b111b3ad14701e302
Aoikaze4 <kaze4border@gmail.com>
2021-06-06 13:59:20 +0000

    [update]book_index.html.erb,some acitons

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,13 +1,34 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
   end
 
   def edit
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.permit(:title,:body)
+    #.require(:book) エラー解決のため一度削除
   end
 end
