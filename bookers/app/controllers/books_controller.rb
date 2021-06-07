6b6ec59e7800110a87c20b61cd868e71124bb015
Aoikaze4 <kaze4border@gmail.com>
2021-06-07 13:59:04 +0000

    [update]create update function

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new(params[:id])
   end
 
   def show
@@ -12,6 +13,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+
+    redirect_to book_path(@book)
   end
 
   def create
@@ -28,7 +37,8 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.permit(:title,:body)
-    #.require(:book) エラー解決のため一度削除
+    params.require(:book).permit(:title,:body)
+    #.require(:book) エラー解決のため一度削除→記載するとUpdateはうまくいく
+    #ないとCreateがうまくいかない…
   end
 end
